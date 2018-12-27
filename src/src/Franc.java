db30ac9fb069cbf62f7b046c9426a474d0c3ba31
Joshua Vaughan <joshcv2112@gmail.com>
2018-12-26 21:48:24 -0700

    Chapter 6: Equality for All, Redux

--- src/src/Franc.java
+++ src/src/Franc.java
@@ -1,19 +1,12 @@
 package src;
 
-public class Franc {
-	
-	private int amount;
-	
+public class Franc extends Money {
+		
 	public Franc(int amount) {
 		this.amount = amount;
 	}
 	
 	public Franc times(int multiplier) {
 		return new Franc(this.amount * multiplier);
-	}
-	
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return this.amount == franc.amount;
 	}
 }
