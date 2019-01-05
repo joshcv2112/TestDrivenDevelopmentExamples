06e726e169938f294b39e5982766d82a86872c5e
Joshua Vaughan <joshcv2112@gmail.com>
2019-01-04 18:06:09 -0700

    The Times were Living in

--- src/src/Money.java
+++ src/src/Money.java
@@ -24,11 +24,11 @@ public class Money {
 	
 	// Factory method
 	public static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 	
 	// Factory method
 	public static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 }
--- src/testCode/TestMultiplication.java
+++ src/testCode/TestMultiplication.java
@@ -21,8 +21,6 @@ public class TestMultiplication extends TestCase {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 	
