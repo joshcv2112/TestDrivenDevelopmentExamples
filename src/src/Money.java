968e5a7efc06b3e3512506b18f7a65c091f8aa9b
Joshua Vaughan <joshcv2112@gmail.com>
2019-01-04 19:06:25 -0700

    Makin Objects

--- src/src/Money.java
+++ src/src/Money.java
@@ -1,7 +1,17 @@
 package src;
 
-public class Money {
+abstract public class Money {
 	protected int amount;
+	
+	abstract public Money times(int multiplier);
+	
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+	
+	public static Money franc(int amount) {
+		return new Dollar(amount);
+	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
--- src/testCode/TestMultiplication.java
+++ src/testCode/TestMultiplication.java
@@ -1,6 +1,7 @@
 package testCode;
 
 import junit.framework.TestCase;
+import src.Money;
 import src.Dollar;
 import src.Franc;
 
@@ -18,11 +19,17 @@ public class TestMultiplication extends TestCase {
 		assertEquals(new Franc(15), five.times(3));
 	}
 	
+	public void testMultiplication() {
+		Money five = Money.dollar(5);
+		assertEquals(Money.dollar(10), five.times(2));
+		assertEquals(Money.dollar(15), five.times(3));
+	}
+	
 	public void testEquality() {
-		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		assertFalse(new Dollar(5).equals(new Dollar(6)));
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
-		assertFalse(new Franc(5).equals(new Dollar(5)));
+		assertFalse(new Franc(5).equals(Money.dollar(5)));
 	}
 }
