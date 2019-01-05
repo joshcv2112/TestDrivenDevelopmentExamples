e78dea247bf467aa4e025fc3104820f978a4e595
Joshua Vaughan <joshcv2112@gmail.com>
2019-01-04 19:29:15 -0700

    rebasing to chapter 8

--- src/src/Money.java
+++ src/src/Money.java
@@ -1,44 +1,21 @@
 package src;
 
-abstract public class Money {
+public abstract class Money {
 	protected int amount;
-	protected String currency;
 	
-	public Money(int amount, String currency) {
-		this.amount = amount;
-		this.currency = currency;
-	}
-
-	public String currency() {
-		return this.currency;
-	}
-	
-	abstract public Money times(int multiplier);
+	public abstract Money times(int multiplier);
 	
 	public static Money dollar(int amount) {
 		return new Dollar(amount);
 	}
 	
 	public static Money franc(int amount) {
-		return new Dollar(amount);
+		return new Franc(amount);
 	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && currency().equals(money.currency());
-	}
-	
-	public Money times(int multiplier) {
-		return new Money(amount * multiplier, currency);
-	}
-	
-	// Factory method
-	public static Money dollar(int amount) {
-		return new Money(amount, "USD");
-	}
-	
-	// Factory method
-	public static Money franc(int amount) {
-		return new Money(amount, "CHF");
+		return this.amount == money.amount
+			&& getClass().equals(money.getClass());
 	}
 }
--- src/testCode/TestMultiplication.java
+++ src/testCode/TestMultiplication.java
@@ -1,24 +1,24 @@
 package testCode;
 
 import junit.framework.TestCase;
-import src.Money;
 import src.Dollar;
 import src.Franc;
+import src.Money;
 
 public class TestMultiplication extends TestCase {
 	
 	public void testDollarMultiplication() {
-		Money five = Money.dollar(5);
-		assertEquals(Money.dollar(10), five.times(2));
-		assertEquals(Money.dollar(15), five.times(3));
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	
 	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
 	}
-	
+
 	public void testMultiplication() {
 		Money five = Money.dollar(5);
 		assertEquals(Money.dollar(10), five.times(2));
@@ -28,15 +28,8 @@ public class TestMultiplication extends TestCase {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertTrue(Money.franc(5).equals(Money.franc(5)));
+		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-	}
-	
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
-	
-	public void testCurrency() {
-		assertEquals("USD", Money.dollar(1).currency());
-		assertEquals("CHF", Money.franc(1).currency());		
 	}
 }
